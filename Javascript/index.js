702a3c0a2869aa59a135b9b2635178bcd9d5c27a
So-Simoneuk <165155995+So-Simoneuk@users.noreply.github.com>
2024-05-08 18:34:07 +0100

    Date added

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,6 +4,6 @@
 let location1Element = document.querySelector ("#location1");
 let location1DateElement = document.querySelector(".date");
 let location1TimeElement = document.querySelector(".time");
-location1DateElement.innerHTML ="Craig";
+location1DateElement.innerHTML = moment().format("MMMM Do YYYY");
 location1TimeElement.innerHTML ="Simone";
 
