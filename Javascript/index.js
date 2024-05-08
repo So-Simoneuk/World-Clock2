8d3611080f85beebe69289edf03b58e3e24983ab
So-Simoneuk <165155995+So-Simoneuk@users.noreply.github.com>
2024-05-08 19:11:04 +0100

    display current time fix

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,8 +1,37 @@
+function updateTime() {
 
-
-let location1Element = document.querySelector ("#location1");
-let location1DateElement = document.querySelector(".date");
-let location1TimeElement = document.querySelector(".time");
-let location1Time = moment();
-location1DateElement.innerHTML = location1Time.format("MMMM Do YYYY");
-location1TimeElement.innerHTML = location1Time.format("h:mm:ss [<small>]A[</small>]");
+    let location1Element = document.querySelector ("#kingston");
+    let location1DateElement = document.querySelector(".date");
+    let location1TimeElement = document.querySelector(".time");
+    let location1Time = moment();
+    location1DateElement.innerHTML = location1Time.format("MMMM Do YYYY");
+    location1TimeElement.innerHTML = location1Time.format("h:mm:ss [<small>]A[</small>]");
+}
+  
+  
+  function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+      "A"
+    )}</small></div>
+    </div>
+    `;
+  }
+  
+  updateTime();
+  setInterval(updateTime, 1000);
+  
+  let citiesSelectElement = document.querySelector("#city");
+  citiesSelectElement.addEventListener("change", updateCity);
