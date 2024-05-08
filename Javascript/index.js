8faf47da0909923cbecfa3b63a0c4a5b292759ff
So-Simoneuk <165155995+So-Simoneuk@users.noreply.github.com>
2024-05-08 20:12:57 +0100

    Code fox

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,13 +1,30 @@
 function updateTime() {
-
-    let location1Element = document.querySelector ("#kingston");
-    let location1DateElement = document.querySelector(".date");
-    let location1TimeElement = document.querySelector(".time");
-    let location1Time = moment();
-    location1DateElement.innerHTML = location1Time.format("MMMM Do YYYY");
-    location1TimeElement.innerHTML = location1Time.format("h:mm:ss [<small>]A[</small>]");
-}
+    // Los Angeles
+    let losAngelesElement = document.querySelector("#los-angeles");
+    if (losAngelesElement) {
+      let losAngelesDateElement = losAngelesElement.querySelector(".date");
+      let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+      let losAngelesTime = moment().tz("America/Los_Angeles");
   
+      losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+      losAngelesTimeElement.innerHTML = losAngelesTime.format(
+        "h:mm:ss [<small>]A[</small>]"
+      );
+    }
+  
+    // Paris
+    let parisElement = document.querySelector("#paris");
+    if (parisElement) {
+      let parisDateElement = parisElement.querySelector(".date");
+      let parisTimeElement = parisElement.querySelector(".time");
+      let parisTime = moment().tz("Europe/Paris");
+  
+      parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+      parisTimeElement.innerHTML = parisTime.format(
+        "h:mm:ss [<small>]A[</small>]"
+      );
+    }
+  }
   
   function updateCity(event) {
     let cityTimeZone = event.target.value;
