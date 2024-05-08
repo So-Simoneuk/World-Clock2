7296f08ac166bea4a1e20c16587729f7729db969
So-Simoneuk <165155995+So-Simoneuk@users.noreply.github.com>
2024-05-08 18:41:11 +0100

    Time and Date updated

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,6 +4,7 @@
 let location1Element = document.querySelector ("#location1");
 let location1DateElement = document.querySelector(".date");
 let location1TimeElement = document.querySelector(".time");
-location1DateElement.innerHTML = moment().format("MMMM Do YYYY");
-location1TimeElement.innerHTML ="Simone";
+let location1Time = moment();
+location1DateElement.innerHTML = location1Time.format("MMMM Do YYYY");
+location1TimeElement.innerHTML = location1Time.format("h:mm:ss [<small>]A[</small>]");
 
