f2ea2e2d92a70f9a1e37a663ee0af0c9f9c5ba0e
So-Simoneuk <165155995+So-Simoneuk@users.noreply.github.com>
2024-05-08 19:02:50 +0100

    time fix fail

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,10 +1,8 @@
 
 
-
 let location1Element = document.querySelector ("#location1");
 let location1DateElement = document.querySelector(".date");
 let location1TimeElement = document.querySelector(".time");
 let location1Time = moment();
 location1DateElement.innerHTML = location1Time.format("MMMM Do YYYY");
 location1TimeElement.innerHTML = location1Time.format("h:mm:ss [<small>]A[</small>]");
-
